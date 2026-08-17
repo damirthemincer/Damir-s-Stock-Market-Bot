66c53fba42e0258cca2dba72bb92e4cad4796247
damirthemincer <damirthemincer@gmail.com>
2026-08-16 22:01:04 -0400

    Created a !company command for user to see more info about a company, and now there will be both ! and / commands

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import schedule
 from pathlib import Path
 import json
+from discord import app_commands
 
 # Load .env
 load_dotenv()
@@ -76,13 +77,48 @@ async def market(ctx):
     await ctx.reply(message)
 
 
-# Slash command
+@bot.command()
+async def company(ctx, target):
+    target = target.lower()
+    check_path = Path(f"companies/{target}.json")
+    if not check_path.is_file():
+        await ctx.reply("Company does not exist, or you made a typo. Please double check your message.")
+        return
+    with open(f"companies/{target}.json", "r") as file:
+        data = json.load(file)
+        message = f"Name: **{data["name"]}** \nDescription: *{data["desc"]}* \nCurrent individual share price: **{str(data["current_stock"])}** \nRisk: **{str(data["risk"])}/10** \nOutstanding Shares: **{str(data["shares"])}**"
+    await ctx.reply(message)
 
+
+# Slash command
 
 @bot.tree.command(name="test", description="Test slash command")
 async def slash_test(interaction: discord.Interaction):
     await interaction.response.send_message("Slash command works!")
 
+
+@bot.tree.command(name="market", description="Views the current stock market!")
+async def market(interaction: discord.Interaction):
+    message = ""
+    for file_path in Path('companies/').iterdir():
+        with open(f"companies/{file_path.name}", "r") as file:
+            data = json.load(file)
+            message += f"\n{data["name"]}: {data["current_stock"]} 💰"
+    await interaction.response.send_message(message)
+
+
+@bot.tree.command(name="company", description="View more details about a specific company!")
+@app_commands.describe(target="Put a companies name, capitalization doesn't matter.")
+async def company(interaction: discord.Interaction, target: str):
+    target = target.lower()
+    check_path = Path(f"companies/{target}.json")
+    if not check_path.is_file():
+        await interaction.response.send_message("Company does not exist, or you made a typo. Please double check your message.")
+        return
+    with open(f"companies/{target}.json", "r") as file:
+        data = json.load(file)
+        message = f"Name: **{data["name"]}** \nDescription: *{data["desc"]}* \nCurrent individual share price: **{str(data["current_stock"])}** \nRisk: **{str(data["risk"])}/10** \nOutstanding Shares: **{str(data["shares"])}**"
+    await interaction.response.send_message(message)
 
 # Run bot
 bot.run(TOKEN)
